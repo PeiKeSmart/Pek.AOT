29d00ae0161a1d0fe205ee41fe26c0462c53a075
猿人易 <qcjxberin@outlook.com>
2026-03-12 19:22:54 +0800

    chore: 更新 XXTraceSample 和 TimerXSample 的项目文件，启用 AOT 和全球化设置

--- Samples/XXTraceSample/Program.cs
+++ Samples/XXTraceSample/Program.cs
@@ -1,57 +1,63 @@
-using Pek.Logging;
-
-var setting = XXTraceSetting.Current;
-var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-var configFile = Path.Combine(baseDirectory, "Config", "Core.config");
-var logDirectory = Path.Combine(baseDirectory, setting.LogPath);
-
-Console.WriteLine("XXTrace Sample");
-Console.WriteLine($"BaseDirectory: {baseDirectory}");
-Console.WriteLine($"ConfigFile: {configFile}");
-Console.WriteLine($"BeforeExists: {File.Exists(configFile)}");
-
-setting.Normalize();
-setting.Save();
-
-Console.WriteLine($"AfterExists: {File.Exists(configFile)}");
-Console.WriteLine("Current Setting Snapshot:");
-Console.WriteLine($"  Debug = {setting.Debug}");
-Console.WriteLine($"  LogLevel = {setting.LogLevel}");
-Console.WriteLine($"  LogPath = {setting.LogPath}");
-Console.WriteLine($"  DataPath = {setting.DataPath}");
+﻿using Pek.Logging;
 
 XXTrace.UseConsole();
-XXTrace.WriteLine("XXTrace sample start");
-XXTrace.WriteLine("Current log level: {0}", setting.LogLevel);
-XXTrace.WriteException(new InvalidOperationException("XXTrace sample exception"));
 
-await Task.Delay(500);
-XXTrace.Shutdown();
+//var setting = XXTraceSetting.Current;
+//var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+//var configFile = Path.Combine(baseDirectory, "Config", "Core.config");
+//var logDirectory = Path.Combine(baseDirectory, setting.LogPath);
 
-Console.WriteLine();
-Console.WriteLine($"LogDirectoryExists: {Directory.Exists(logDirectory)}");
+//Console.WriteLine("XXTrace Sample");
+//Console.WriteLine($"BaseDirectory: {baseDirectory}");
+//Console.WriteLine($"ConfigFile: {configFile}");
+//Console.WriteLine($"BeforeExists: {File.Exists(configFile)}");
 
-if (Directory.Exists(logDirectory))
-{
-    var logFiles = Directory.GetFiles(logDirectory, "*.log", SearchOption.TopDirectoryOnly)
-        .OrderByDescending(e => e)
-        .ToArray();
+//setting.Normalize();
+//setting.Save();
 
-    Console.WriteLine($"LogFileCount: {logFiles.Length}");
-    if (logFiles.Length > 0)
-    {
-        Console.WriteLine($"LatestLogFile: {logFiles[0]}");
-        Console.WriteLine("LatestLogContent:");
-        Console.WriteLine(File.ReadAllText(logFiles[0]));
-    }
-}
+//Console.WriteLine($"AfterExists: {File.Exists(configFile)}");
+//Console.WriteLine("Current Setting Snapshot:");
+//Console.WriteLine($"  Debug = {setting.Debug}");
+//Console.WriteLine($"  LogLevel = {setting.LogLevel}");
+//Console.WriteLine($"  LogPath = {setting.LogPath}");
+//Console.WriteLine($"  DataPath = {setting.DataPath}");
 
-if (File.Exists(configFile))
-{
-    Console.WriteLine();
-    Console.WriteLine("Core.config Content:");
-    Console.WriteLine(File.ReadAllText(configFile));
-}
+//XXTrace.UseConsole();
+//XXTrace.WriteLine("XXTrace sample start");
+//XXTrace.WriteLine("Current log level: {0}", setting.LogLevel);
+//XXTrace.WriteException(new InvalidOperationException("XXTrace sample exception"));
 
-Console.WriteLine();
-Console.WriteLine("Done.");
+//await Task.Delay(500);
+//XXTrace.Shutdown();
+
+//Console.WriteLine();
+//Console.WriteLine($"LogDirectoryExists: {Directory.Exists(logDirectory)}");
+
+//if (Directory.Exists(logDirectory))
+//{
+//    var logFiles = Directory.GetFiles(logDirectory, "*.log", SearchOption.TopDirectoryOnly)
+//        .OrderByDescending(e => e)
+//        .ToArray();
+
+//    Console.WriteLine($"LogFileCount: {logFiles.Length}");
+//    if (logFiles.Length > 0)
+//    {
+//        Console.WriteLine($"LatestLogFile: {logFiles[0]}");
+//        Console.WriteLine("LatestLogContent:");
+//        Console.WriteLine(File.ReadAllText(logFiles[0]));
+//    }
+//}
+
+//if (File.Exists(configFile))
+//{
+//    Console.WriteLine();
+//    Console.WriteLine("Core.config Content:");
+//    Console.WriteLine(File.ReadAllText(configFile));
+//}
+
+//Console.WriteLine();
+//Console.WriteLine("Done.");
+
+XXTrace.WriteLine($"开始测试");
+
+Console.ReadKey();
