f4374637a49e6ffc2e5bb11b5e91fff605f6bdd3
猿人易 <qcjxberin@outlook.com>
2026-05-12 08:11:05 +0800

    feat: 添加 Socket 服务器集合和会话管理功能，优化网络服务器结构

--- Pek.AOT/Net/NetServer.cs
+++ Pek.AOT/Net/NetServer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Net;
 using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
@@ -11,7 +12,7 @@ using Pek.Model;
 namespace Pek.Net;
 
 /// <summary>网络服务器。可同时支持多个Socket服务器，同时支持IPv4和IPv6，同时支持Tcp和Udp</summary>
-public class NetServer : DisposeBase, IExtend, ILogFeature
+public class NetServer : DisposeBase, IServer, IExtend, ILogFeature
 {
     #region 属性
     /// <summary>服务名</summary>
@@ -40,6 +41,22 @@ public class NetServer : DisposeBase, IExtend, ILogFeature
     /// <summary>地址族</summary>
     public AddressFamily AddressFamily { get; set; }
 
+    /// <summary>底层Socket服务器集合</summary>
+    public IList<ISocketServer> Servers { get; private set; } = [];
+
+    /// <summary>默认Socket服务器</summary>
+    public ISocketServer? Server
+    {
+        get => Servers.Count > 0 ? Servers[0] : null;
+        set
+        {
+            if (value == null)
+                Servers.Clear();
+            else if (!Servers.Contains(value))
+                Servers.Insert(0, value);
+        }
+    }
+
     /// <summary>是否活动</summary>
     public Boolean Active { get; protected set; }
 
@@ -67,6 +84,9 @@ public class NetServer : DisposeBase, IExtend, ILogFeature
     /// <summary>Socket层性能追踪器</summary>
     public ITracer? SocketTracer { get; set; }
 
+    /// <summary>Socket层日志</summary>
+    public ILog? SocketLog { get; set; }
+
     /// <summary>是否输出发送日志</summary>
     public Boolean LogSend { get; set; }
 
@@ -88,6 +108,21 @@ public class NetServer : DisposeBase, IExtend, ILogFeature
 
     /// <summary>日志</summary>
     public ILog Log { get; set; } = Logger.Null;
+
+    private readonly ConcurrentDictionary<Int32, INetSession> _sessions = new();
+
+    /// <summary>会话集合</summary>
+    public IDictionary<Int32, INetSession> Sessions => _sessions;
+
+    private Int32 _sessionCount;
+
+    /// <summary>当前会话数</summary>
+    public Int32 SessionCount { get => _sessionCount; set => _sessionCount = value; }
+
+    private Int32 _maxSessionCount;
+
+    /// <summary>最高会话数</summary>
+    public Int32 MaxSessionCount => _maxSessionCount;
     #endregion
 
     #region 构造
@@ -98,9 +133,293 @@ public class NetServer : DisposeBase, IExtend, ILogFeature
 
         if (SocketSetting.Current.Debug) Log = XTrace.Log;
     }
+
+    /// <summary>通过指定端口实例化一个网络服务器</summary>
+    /// <param name="port">监听端口</param>
+    public NetServer(Int32 port) : this(IPAddress.Any, port) { }
+
+    /// <summary>通过指定监听地址和端口实例化一个网络服务器</summary>
+    /// <param name="address">监听地址</param>
+    /// <param name="port">监听端口</param>
+    public NetServer(IPAddress address, Int32 port) : this(address, port, NetType.Unknown) { }
+
+    /// <summary>通过指定监听地址、端口和协议实例化一个网络服务器</summary>
+    /// <param name="address">监听地址</param>
+    /// <param name="port">监听端口</param>
+    /// <param name="protocolType">协议类型</param>
+    public NetServer(IPAddress address, Int32 port, NetType protocolType) : this()
+    {
+        Local.Address = address;
+        Port = port;
+        Local.Type = protocolType;
+    }
+
+    /// <summary>释放资源</summary>
+    /// <param name="disposing">是否显式释放</param>
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        if (Active) Stop(GetType().Name + (disposing ? "Dispose" : "GC"));
+    }
     #endregion
 
     #region 方法
+    /// <summary>添加Socket服务器</summary>
+    /// <param name="server">要添加的Socket服务器</param>
+    /// <returns>是否成功</returns>
+    public virtual Boolean AttachServer(ISocketServer server)
+    {
+        if (Servers.Contains(server)) return false;
+
+        server.Name = Name;
+        server.NewSession += Server_NewSession;
+
+        if (SessionTimeout > 0) server.SessionTimeout = SessionTimeout;
+        if (Pipeline != null) server.Pipeline = Pipeline;
+
+        server.Log = SocketLog ?? Log;
+        if (SocketTracer != null) server.Tracer = SocketTracer;
+        server.LogSend = LogSend;
+        server.LogReceive = LogReceive;
+        server.Error += OnError;
+
+        Servers.Add(server);
+        return true;
+    }
+
+    /// <summary>添加管道处理器</summary>
+    /// <typeparam name="THandler">处理器类型</typeparam>
+    public void Add<THandler>() where THandler : IPipelineHandler, new() => GetPipe().Add(new THandler());
+
+    /// <summary>添加管道处理器</summary>
+    /// <param name="handler">处理器实例</param>
+    public void Add(IPipelineHandler handler) => GetPipe().Add(handler);
+
+    private IPipeline GetPipe() => Pipeline ??= new Pipeline();
+
+    /// <summary>开始服务</summary>
+    public void Start()
+    {
+        if (Active) return;
+        if (Servers.Count == 0)
+        {
+            this.WriteLog("没有可用Socket服务器！");
+            return;
+        }
+
+        foreach (var item in Servers)
+        {
+            item.Start();
+            this.WriteLog("开始监听 {0}", item);
+        }
+
+        if (Server != null) Local.Type = Server.Local.Type;
+        Active = true;
+        this.WriteLog("准备就绪！");
+    }
+
+    /// <summary>停止服务</summary>
+    /// <param name="reason">关闭原因</param>
+    public void Stop(String? reason)
+    {
+        var activeServers = Servers.Where(e => e.Active).ToArray();
+        if (activeServers.Length == 0)
+        {
+            Active = false;
+            return;
+        }
+
+        if (String.IsNullOrEmpty(reason)) reason = GetType().Name + "Stop";
+
+        foreach (var item in activeServers)
+        {
+            this.WriteLog("停止监听 {0}", item);
+            item.Stop(reason);
+        }
+
+        if (_sessions.Count > 0)
+        {
+            foreach (var item in _sessions.Values.ToArray())
+            {
+                item.TryDispose();
+            }
+
+            _sessions.Clear();
+        }
+
+        Active = false;
+        this.WriteLog("已停止！");
+    }
+
+    /// <summary>新会话事件</summary>
+    public event EventHandler<NetSessionEventArgs>? NewSession;
+
+    /// <summary>数据接收事件</summary>
+    public event EventHandler<ReceivedEventArgs>? Received;
+
+    /// <summary>错误事件</summary>
+    public event EventHandler<ExceptionEventArgs>? Error;
+
+    private void Server_NewSession(Object? sender, SessionEventArgs e)
+    {
+        var session = OnNewSession(e.Session);
+        NewSession?.Invoke(sender, new NetSessionEventArgs { Session = session });
+    }
+
+    protected virtual INetSession OnNewSession(ISocketSession session)
+    {
+        var count = Interlocked.Increment(ref _sessionCount);
+        session.OnDisposed += (s, e) => Interlocked.Decrement(ref _sessionCount);
+
+        var max = _maxSessionCount;
+        while (count > max)
+        {
+            if (Interlocked.CompareExchange(ref _maxSessionCount, count, max) == max) break;
+            max = _maxSessionCount;
+        }
+
+        var netSession = CreateSession(session);
+        if (netSession is NetSession typedSession)
+        {
+            typedSession.ID = Interlocked.Increment(ref _sessionID);
+            typedSession.Log = Log;
+        }
+
+        netSession.Host = this;
+        netSession.Server = session.Server;
+        netSession.Session = session;
+
+        if (UseSession) AddSession(netSession);
+
+        netSession.Received += OnReceived;
+        netSession.Start();
+
+        return netSession;
+    }
+
+    private Int32 _sessionID;
+
+    protected virtual void OnReceived(Object? sender, ReceivedEventArgs e)
+    {
+        if (sender is INetSession session)
+        {
+            if (e.Packet != null) OnReceive(session, e.Packet);
+            OnReceive(session, e);
+        }
+
+        Received?.Invoke(sender, e);
+    }
+
+    /// <summary>收到数据包时的处理</summary>
+    /// <param name="session">网络会话</param>
+    /// <param name="packet">数据包</param>
+    protected virtual void OnReceive(INetSession session, IPacket packet) { }
+
+    /// <summary>收到数据时的处理</summary>
+    /// <param name="session">网络会话</param>
+    /// <param name="e">接收事件参数</param>
+    protected virtual void OnReceive(INetSession session, ReceivedEventArgs e) { }
+
+    protected virtual void OnError(Object? sender, ExceptionEventArgs e)
+    {
+        if (Log.Enable) Log.Error("{0} Error {1}", sender, e.Exception);
+
+        Error?.Invoke(sender, e);
+    }
+
+    protected virtual void AddSession(INetSession session)
+    {
+        session.Host = this;
+
+        if (_sessions.TryAdd(session.ID, session))
+        {
+            session.OnDisposed += (s, e) =>
+            {
+                if (s is INetSession netSession) _sessions.TryRemove(netSession.ID, out _);
+            };
+        }
+        else if (Log.Enable)
+        {
+            Log.Warn("会话已存在，忽略重复添加。ID={0}", session.ID);
+        }
+    }
+
+    protected virtual INetSession CreateSession(ISocketSession session)
+    {
+        var netSession = new NetSession
+        {
+            Server = session.Server,
+            Session = session
+        };
+        ((INetSession)netSession).Host = this;
+
+        return netSession;
+    }
+
+    /// <summary>根据会话ID查找会话</summary>
+    /// <param name="sessionID">会话ID</param>
+    /// <returns>网络会话</returns>
+    public INetSession? GetSession(Int32 sessionID)
+    {
+        if (sessionID == 0) return null;
+
+        return _sessions.TryGetValue(sessionID, out var session) ? session : null;
+    }
+
+    /// <summary>异步群发数据给所有客户端</summary>
+    /// <param name="data">数据包</param>
+    /// <param name="predicate">过滤器</param>
+    /// <returns>发送数量</returns>
+    public virtual Task<Int32> SendAllAsync(IPacket data, Func<INetSession, Boolean>? predicate = null)
+    {
+        if (!UseSession) throw new ArgumentOutOfRangeException(nameof(UseSession), true, "Mass posting requires the use of session collections");
+
+        var count = 0;
+        foreach (var session in _sessions.Values)
+        {
+            if (predicate != null && !predicate(session)) continue;
+
+            try
+            {
+                session.Send(data);
+                count++;
+            }
+            catch { }
+        }
+
+        return Task.FromResult(count);
+    }
+
+    /// <summary>异步群发数据给所有客户端</summary>
+    /// <param name="data">数据包</param>
+    /// <returns>发送数量</returns>
+    public virtual Task<Int32> SendAllAsync(IPacket data) => SendAllAsync(data, null);
+
+    /// <summary>群发消息给所有客户端</summary>
+    /// <param name="message">消息</param>
+    /// <param name="predicate">过滤器</param>
+    /// <returns>发送数量</returns>
+    public virtual Int32 SendAllMessage(Object message, Func<INetSession, Boolean>? predicate = null)
+    {
+        if (!UseSession) throw new ArgumentOutOfRangeException(nameof(UseSession), true, "Mass posting requires the use of session collections");
+
+        var count = 0;
+        foreach (var session in _sessions.Values)
+        {
+            if (predicate != null && !predicate(session)) continue;
+
+            try
+            {
+                session.SendMessage(message);
+                count++;
+            }
+            catch { }
+        }
+
+        return count;
+    }
+
     /// <summary>为会话创建网络数据处理器</summary>
     /// <remarks>可作为业务处理实现，也可以作为前置协议解析。子类可重载返回自定义处理器</remarks>
     /// <param name="session">网络会话</param>
